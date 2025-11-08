d6839a99bd05dbdbfb51b5014cb0dc438acf594f
Sirat Kaur <143172141+harsiratvirk@users.noreply.github.com>
2025-11-08 22:34:33 +0100

    Fix Vite config and update App routing with auth pages

--- healthcalendar/src/App.tsx
+++ healthcalendar/src/App.tsx
@@ -2,6 +2,10 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import HomePage from './home/HomePage'
+import EventCalendar from './patientComponents/EventCalendar'
+import LoginPage from './auth/LoginPage'
+import RegistrationPage from './auth/RegistrationPage.tsx'
+import WorkerLoginPage from './auth/WorkerLoginPage.tsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -10,6 +14,10 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/patient/events' element={<EventCalendar />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegistrationPage />} />
+        <Route path='/worker/login' element={<WorkerLoginPage />} />
         <Route path='*' element={<HomePage />} />
       </Routes>
     </Router>
